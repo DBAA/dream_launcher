b89264698dfb62ce7e76bc0fadd476035d08744b
Andy Wallace <andymasteroffish@gmail.com>
2018-05-16 14:47:52 -0400

    some background work

---
+++ dream_launcher/src/Background.hpp
@@ -0,0 +1,29 @@
+//
+//  Background.hpp
+//  dream_jam_tetsing
+//
+//  Created by Andrew Wallace on 5/16/18.
+//
+
+#ifndef Background_hpp
+#define Background_hpp
+
+#include "ofMain.h"
+
+class Background{
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    
+    vector<ofColor> colors;
+    ofImage barPic;
+    
+    int offsetInt;
+    float offsetY;
+    
+    
+};
+
+#endif /* Background_hpp */
